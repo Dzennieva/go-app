9fbfc83092823e5a875d9a316c3f4e58ffc5f0ff
Jennifer Ajibo <jenniferajibo@gmail.com>
2025-06-22 16:27:03 +0100

    updated static files

--- main_test.go
+++ main_test.go
@@ -1,5 +1,4 @@
-// Test the main function
-
+// main_test.go
 package main
 
 import (
@@ -8,26 +7,19 @@ import (
 	"testing"
 )
 
-func TestMain(t *testing.T) {
-	req, err := http.NewRequest("GET", "/home", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+func TestHomePageHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/home", nil)
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(homePage)
 
+	handler := http.HandlerFunc(homePage)
 	handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+	if rr.Code != http.StatusOK {
+		t.Errorf("unexpected status code: got %d, want %d", rr.Code, http.StatusOK)
 	}
 
-	// Just verify the code not html content
-	expected := "text/html; charset=utf-8"
-	if contentType := rr.Header().Get("Content-Type"); contentType != expected {
-		t.Errorf("handler returned unexpected content type: got %v want %v",
-			contentType, expected)
+	expectedContentType := "text/html; charset=utf-8"
+	if contentType := rr.Header().Get("Content-Type"); contentType != expectedContentType {
+		t.Errorf("unexpected content type: got %s, want %s", contentType, expectedContentType)
 	}
 }
